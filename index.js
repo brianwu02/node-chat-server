3b9ffb41f4e3c86a5ee3359bc055ce0acc014fb2
Brian Wu <brianwu02@gmail.com>
2014-02-26 15:29:17 -0500

    adding socket.io portion to app.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.get('/', function(req, res) {
     res.send("It works!");
 });
 
-app.listen(port);
+// we pass the expressJS server to socket.io. in effect, 
+// our real time communication will still happen on same port.
+var io = require('socket.io').listen(app.listen(port));
+
 console.log('listening on port: ' + port);
 
 app.set('views', __dirname + '/template');
@@ -18,4 +21,18 @@ app.get('/', function(req, res) {
     res.render("page");
 });
 
+app.use(express.static(__dirname + '/public'));
+
+
+// portion of code that will recieve a message from 
+// the client and send it to all the others. 
+// Every socket.io application begins with a 
+// 'connection' handler
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function(data) {
+        io.sockets.emit('message', data);
+    });
+});
 
